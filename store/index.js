bbf30b4acf11f8c564348902a87f6848b28d6862
eezing <eric@ezinterweb.com>
2017-02-15 01:31:39 -0800

    moved redux-dev-tools to dependencies

--- store/index.js
+++ store/index.js
@@ -1,6 +1,6 @@
 
 import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import reducer from './reducer';
 import middleware from './middleware';
 
